003f6786b424ce6b781111b75fe17bca27b8ba8b
gaura <gaura@LAPTOP-SJ8DN68H>
2019-06-26 17:17:31 +0530

    sadf

--- src/test/java/automation/session/WebDriverFactory.java
+++ src/test/java/automation/session/WebDriverFactory.java
@@ -87,6 +87,7 @@ public class WebDriverFactory {
 		//options.addArguments("--start-fullscreen");
 		options.addArguments("--disable-extensions");
 		options.addArguments("--start-maximized");
+		options.addArguments("--disable-dev-shm-usage");
 		Map<String, Object> prefs = new HashMap<String, Object>();
 		
 		prefs.put("profile.default_content_settings.popups", 0);
