d55933554e789b1f94d60a88dda1f01084cd71b6
gaura <gaura@LAPTOP-SJ8DN68H>
2019-06-26 18:31:54 +0530

    jnj

--- src/test/java/automation/session/WebDriverFactory.java
+++ src/test/java/automation/session/WebDriverFactory.java
@@ -83,10 +83,12 @@ public class WebDriverFactory {
 		ChromeOptions options = new ChromeOptions();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
 		
-		options.addArguments("test-type");
+		//options.addArguments("test-type");
 		//options.addArguments("--start-fullscreen");
-		//options.addArguments("--disable-extensions");
-		options.addArguments("--start-maximized");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--headless");
+		options.addArguments("--disable-gpu");
+		//options.addArguments("--start-maximized");
 		//options.addArguments("--disable-dev-shm-usage");
 		options.addArguments("--no-sandbox");
 		
